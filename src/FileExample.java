ea65d65599eac56030e0bdf97b163d9650e96411
Adityagupta33 <aditya.gupta0504@gmail.com>
2024-02-14 20:59:34 +0530

    created

--- src/FileExample.java
+++ src/FileExample.java
@@ -1,31 +1,55 @@
 // In this program we would try to implement file output and input streams.
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class FileExample {
-    public static void main(String[] args) {
-        try{
-            FileOutputStream fos = new FileOutputStream("C:/MyJava/Test2.txt");
-            String str = "Learn Java programming";
+//public class FileExample {
+//    public static void main(String[] args) {
+//        try{
+//            FileOutputStream fos = new FileOutputStream("C:/MyJava/Test2.txt");
+//            String str = "Learn Java programming";
+//
+////            fos.write(str.getBytes());
+//
+//            byte b[] = str.getBytes();
+////            for(byte x:b){
+////                fos.write(x);
+////            }
+//
+//            fos.write(b,6,str.length()-6);
+//
+//            fos.close();
+//        }
+//        catch(FileNotFoundException e){
+//            System.out.println(e);
+//        }
+//        catch (IOException e){
+//            System.out.println("The Exception is "+e);
+//        }
+//
+//    }
+//}
 
-//            fos.write(str.getBytes());
+public class FileExample{
+    public static void main(String[] args) throws Exception {
+        try(FileInputStream fis = new FileInputStream("C:/MyJava/Test.txt")){
+//            byte b[] = new byte[fis.available()];
+//
+//            fis.read(b);
+//            String str = new String(b);
+//            System.out.println(str);
 
-            byte b[] = str.getBytes();
-//            for(byte x:b){
-//                fos.write(x);
-//            }
+            int x;
 
-            fos.write(b,6,str.length()-6);
+            do{
+                x = fis.read();
+                if(x!= -1){
+                    System.out.print((char) x);
+                }
+            }while (x!= -1);
 
-            fos.close();
-        }
-        catch(FileNotFoundException e){
-            System.out.println(e);
-        }
-        catch (IOException e){
-            System.out.println("The Exception is "+e);
         }
 
     }
