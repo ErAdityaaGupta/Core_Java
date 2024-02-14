d14f3629b223e391f09326fc7df2e63f8ce69ef6
Adityagupta33 <aditya.gupta0504@gmail.com>
2024-02-14 21:34:16 +0530

    created

--- src/ByteDemo.java
+++ src/ByteDemo.java
@@ -2,6 +2,7 @@
 
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 //public class ByteDemo {
 //    public static void main(String[] args)throws Exception {
@@ -24,7 +25,23 @@ import java.io.ByteArrayInputStream;
 //}
 
 public class ByteDemo{
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
+        byte b[] = {'a','b','c','d','e','f','g','h','i','j'};
 
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(20);
+        bos.write('a');
+        bos.write('b');
+        bos.write('c');
+        bos.write('d');
+        bos.write('e');
+
+        byte c[] = bos.toByteArray();
+
+        for (byte x:c){
+            System.out.print((char) x + " ");
+        }
+
+
+        bos.close();
     }
 }
