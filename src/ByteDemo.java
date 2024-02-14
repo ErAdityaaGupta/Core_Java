6ccd9067e57a666dfa4c4e44c9765f82cc7da473
Adityagupta33 <aditya.gupta0504@gmail.com>
2024-02-14 21:26:06 +0530

    created

---
+++ src/ByteDemo.java
@@ -0,0 +1,30 @@
+// In this program we use methods of byte array output and input streams.
+
+
+import java.io.ByteArrayInputStream;
+
+//public class ByteDemo {
+//    public static void main(String[] args)throws Exception {
+//        byte b[] = {'a','b','c','d','e','f','g','h','i','j'};
+//
+//        ByteArrayInputStream bis = new ByteArrayInputStream(b);
+//
+////        int x =0;
+////
+////        while((x=bis.read())!= -1){
+////            System.out.print((char) x );
+////        }
+//
+//        String str = new String(bis.readAllBytes());
+//        System.out.println(str);
+//        System.out.println(bis.markSupported());
+//        bis.close();
+//
+//    }
+//}
+
+public class ByteDemo{
+    public static void main(String[] args) {
+
+    }
+}
