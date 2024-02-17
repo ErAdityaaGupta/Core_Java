46b96f0a516b9b5c686aac69314cc6affca92cb1
Adityagupta33 <aditya.gupta0504@gmail.com>
2024-02-17 21:53:55 +0530

    created

--- src/ListDemo.java
+++ src/ListDemo.java
@@ -12,11 +12,26 @@ public class ListDemo {
         al1.add(10);
         al1.add(0,5);
         al1.addAll(1,al2);
+        al1.add(5,70);
 
+        System.out.println(al1.contains(6));
+        System.out.println(al1.get(5));
+        System.out.println(al1.indexOf(70));
+        System.out.println(al1.lastIndexOf(70));
 
-
+        al1.set(6,100);
         System.out.println(al1);
+        for (int i= 0;i<al1.size();i++){
+            System.out.print(al1.get(i)+" ");
+        }
 
+        for (Integer integer : al1) {
+            System.out.print(integer + " ");
+        }
+        System.out.println(" ");
+        for(Integer x:al1){
+            System.out.print(x+" ");
+        }
 
     }
 }
